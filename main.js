5109dddd1c545e90acd6292af82e0e323f5ad3df
corni <gmigan.a@gmail.com>
2024-07-13 15:42:04 +0200

    feature: process command line argument

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+function main() {
+	if (process.argv.length < 3) {
+		console.error('There is not enough arguments.')
+		return 1
+	} else if (process.argv.length > 3) {
+		console.error('Too many arguments.')
+		return 1
+	} else {
+		console.log('The crawler is starting...')
+	}
+}
+
+main()
